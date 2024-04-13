4f9640e6bab449a192cb35e8d02e0627183653ae
ztrackperu <ztrack@zgroup.com.pe>
2024-04-13 09:35:13 -0500

    base 5

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
+#from fastapi import FastAPI
+
+#app = FastAPI()
+
+#app.title = "Mi primera aplicacion con FastAPI"
+#app.version ="2.0"
+#@app.get('/',tags=['Home'])
+
+#def home():
+    #return "Hola mundo luis!"
+#@app.get('/home',tags=['Home'])
+
+#def home():
+    #return "Hola mundo luis!"
+
+
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-app.title = "Mi primera aplicacion con FastAPI"
-app.version ="2.0"
-@app.get('/',tags=['Home'])
 
-def home():
-    return "Hola mundo luis!"
-@app.get('/home',tags=['Home'])
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
-def home():
-    return "Hola mundo luis!"
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
