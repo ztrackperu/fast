14fada2b82604ed78cde1a2fc7185d851d3b56de
lpnoobx <lpnoobx@gmail.com>
2024-04-13 06:59:24 -0500

    base2

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+app.title = "Mi primera aplicacion con FastAPI"
+app.version ="2.0"
+@app.get('/',tags=['Home'])
 
 def home():
-    return "Hola mundo !"
+    return "Hola mundo luis!"
+@app.get('/home',tags=['Home'])
+
+def home():
+    return "Hola mundo luis!"
