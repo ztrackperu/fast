0acd299afd67f7f5708d82d0cf6f3084c663bfc6
ztrackperu <ztrack@zgroup.com.pe>
2024-04-15 12:20:19 -0500

    va6

--- src/main.py
+++ src/main.py
@@ -12,25 +12,18 @@ import datetime
 
 app = FastAPI()
 
-app.include_router(router=movie_router)
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
-
 @app.get("/",tags=['Home'])
 def home():
     #return {"Hello": "World"}
     return PlainTextResponse(content='Home Luis',status_code=200)
 
 
-
 @app.get('/get_file')
 def get_file():
     return FileResponse('file.pdf')
+
+app.include_router(prefix='/movies',router=movie_router)
+
 
 
 
--- src/routers/movie_router.py
+++ src/routers/movie_router.py
@@ -7,13 +7,13 @@ movies:List[Movie] =[]
 
 movie_router = APIRouter()
 
-@movie_router.get('/movies',tags=['Movies'],status_code=200,response_description="Nos debe devolver una respuesta exitosa")
+@movie_router.get('/',tags=['Movies'],status_code=200,response_description="Nos debe devolver una respuesta exitosa")
 def get_movies()->List[Movie]:
     content = [movie.model_dump() for movie in movies]
     return JSONResponse(content=content,status_code=200)
 
 
-@movie_router.get('/movies/{id}',tags=['Movies'])
+@movie_router.get('/{id}',tags=['Movies'])
 def get_movie(id:int=Path(gt=0))->Movie | dict: 
     for movie in movies :
         if movie.id==id:
@@ -22,20 +22,20 @@ def get_movie(id:int=Path(gt=0))->Movie | dict:
 
 
 
-@movie_router.get('/movies/',tags=['Movies'])
+@movie_router.get('/by_category',tags=['Movies'])
 def get_movie_by_category(category:str = Query(min_length=5,max_length=20))->Movie | dict:
     for movie in movies :
         if movie.category==category:
             return JSONResponse(content=movie.model_dump(),status_code=200)      
     return JSONResponse(content={},status_code=404)    
 
-@movie_router.post('/movies' , tags=['Movies'])
+@movie_router.post('/' , tags=['Movies'])
 def create_movie(movie:MovieCreate)->List[Movie]:
     movies.append(movie)
     content = [movie.model_dump() for movie in movies]
     return JSONResponse(content=content,status_code=201)
 
-@movie_router.put('/movies/{id}',tags=['Movies'])
+@movie_router.put('/{id}',tags=['Movies'])
 def update_movie(id:int,movie1:MovieUpdate)->List[Movie]:
     for movie in movies :
         if movie.id==id:
@@ -48,7 +48,7 @@ def update_movie(id:int,movie1:MovieUpdate)->List[Movie]:
     return JSONResponse(content=content,status_code=200)
 
 
-@movie_router.delete('/movies/{id}',tags=['Movies'])
+@movie_router.delete('/{id}',tags=['Movies'])
 def delete_movie(id:int)->List[Movie]:
     for movie in movies :
         if movie.id==id:
