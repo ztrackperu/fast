baeb1b0751dabbaf77c9140b4df8edd3b1a04c31
ztrackperu <ztrack@zgroup.com.pe>
2024-04-15 09:10:30 -0500

    base 28

--- main.py
+++ main.py
@@ -63,11 +63,11 @@ class Movie(BaseModel):
 class MovieCreate(BaseModel):
     id:int
     title:str=Field(min_length=5,max_length=15,default="My Movie")
-    overview:str =Field(min_length=15,max_length=58)
+    overview:str =Field(min_length=15,max_length=58,default="Esta pelicula trata acerca de ...")
     #año menor o igual al año actual y menor igual a 1900
-    year:int = Field(le=datetime.date.today().year,ge=1900)
-    rating:float =Field(ge=0,le=10)
-    category:str = Field(min_length=5,max_length=20)
+    year:int = Field(le=datetime.date.today().year,ge=1900 , default=2023)
+    rating:float =Field(ge=0,le=10, default=10)
+    category:str = Field(min_length=5,max_length=20,default="Acción")
 
 #para validar numeros 
 
