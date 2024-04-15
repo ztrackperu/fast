8c946a20fc7c5112cb9e5210d7bc57ebc84ec0f0
ztrackperu <ztrack@zgroup.com.pe>
2024-04-15 14:51:30 -0500

    va9

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI 
-from fastapi.responses import PlainTextResponse,FileResponse
+from fastapi.requests import Request
+from fastapi.responses import PlainTextResponse,FileResponse,Response,JSONResponse
 #importamos la ruta de movie
 from src.routers.movie_router import movie_router
 from src.utils.http_error_handler import HTTPErrorHandler
 app = FastAPI()
 #AQUI TRAEMOS EL MIDDLEWARE
-app.add_middleware(HTTPErrorHandler)
+#app.add_middleware(HTTPErrorHandler)
+@app.middleware('http')
+async def http_error_handler(request:Request,call_next)->Response | JSONResponse:
+    print('Middleware is running!')
+    return await call_next(request)
 
 @app.get("/",tags=['Home'])
 def home():
