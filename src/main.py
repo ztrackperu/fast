f1517948763989f8076147f134c277391601136f
ztrackperu <ztrack@zgroup.com.pe>
2024-04-17 09:52:32 -0500

    con mensaje corregidos16

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ app = FastAPI(dependencies=[Depends(dependency1),Depends(dependency2)])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
