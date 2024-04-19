a9452bd10b8025bd553de1945d1a71e8bbbda2f6
lpnoobx <lpnoobx@gmail.com>
2024-04-19 06:27:22 -0500

    tab_unid8

--- src/main.py
+++ src/main.py
@@ -300,6 +300,6 @@ def ListaUnidadMedida():
     item_details = tab_unid.aggregate(pip)
     for item in item_details :
         #content1.append(item)
-        return JSONResponse(content=item,status_code=200) 
+        return JSONResponse(content=dict(item),status_code=200) 
 
 
